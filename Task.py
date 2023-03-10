1ae6afcbcfd2429d7e1c6c058d0867883d403f92
AlekhinVladimir <Alehin.v.m@mail.com>
2023-03-10 15:40:34 +0400

    Task30

--- Task.py
+++ Task.py
@@ -1,11 +1,8 @@
 import random
-length_mass=int(input('Введите длину массива: '))
-search_number=int(input('Введите искомое число: '))
-my_list=[random.randint(1,20) for _ in range(length_mass)]
-print(f'Массив: {my_list}')
-min_delta=max(my_list)
-for index in range(length_mass):
-    if abs(search_number-my_list[index])<min_delta:
-               min_delta=abs(search_number-my_list[index])
-               search_number=my_list[index]
-print(f'Максимально близкое к искомому числу - число {min_delta} встречается {my_list.count(search_number)} раз.')           
+
+my_list1=[random.randint(1,20) for _ in range(10)]
+my_list2=[random.randint(1,20) for _ in range(10)]
+print(my_list1)
+print(my_list2)
+
+
