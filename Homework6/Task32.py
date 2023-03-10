4a2b572d4878702e389044464487218f05c016bf
AlekhinVladimir <Alehin.v.m@mail.com>
2023-03-10 15:48:05 +0400

    Task32

--- Homework6/Task32.py
+++ Homework6/Task32.py
@@ -6,3 +6,10 @@
 # 4, -2, 10, 2, 0, -9, 8, 10, -9,
 # 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
+
+import random
+a=int(input('Введите минимальное значение диапазона: '))
+b=int(input('Введите максимальное значение диапазона: '))
+length_massive=int(input('Введите длину массива:'))
+my_list=[random.randint(-20,20) for _ in range(length_massive)]
+print(my_list)
