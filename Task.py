c1d18a41ba4aa54e6161d7eb4ff055fb50c07df2
AlekhinVladimir <Alehin.v.m@mail.com>
2023-03-20 14:16:21 +0400

    text_fields

--- Task.py
+++ Task.py
@@ -1,8 +1,10 @@
-import random
+# import random
 
-my_list1=[random.randint(1,20) for _ in range(10)]
-my_list2=[random.randint(1,20) for _ in range(10)]
-print(my_list1)
-print(my_list2)
+# my_list1=[random.randint(1,20) for _ in range(10)]
+# my_list2=[random.randint(1,20) for _ in range(10)]
+# print(my_list1)
+# print(my_list2)
 
-
+a = 50
+b = 100
+print(a + a + float(b))
